0812c6cda177f18b5cf8be548bd831e59dc89551
Marc N <git@marcnitzsche.de>
2022-10-07 17:15:45 +0200

    Add <Head> to every page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
@@ -7,9 +8,19 @@ const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Learn Foreign Numbers</title>
+        <meta
+          name="description"
+          content="Learn Foreign Numbers In Any Language"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
